70f74d63f967f072eb0a2bb1ea6fd1e341c8f29f
RYLiang18 <richard.y.liang@gmail.com>
2021-01-07 21:49:40 -0800

    navbar satisfactory

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,80 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, {useState} from "react"
+import {
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  NavbarToggler,
+  Collapse,
+  NavLink
+} from 'reactstrap'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return(
+    <Navbar light color="light" expand="lg">
+      <NavbarBrand><Link to="/">Richard Liang</Link></NavbarBrand>
+
+      <NavbarToggler onClick = {toggle}/>
+      <Collapse
+        isOpen = {isOpen}
+        navbar
+      >
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink>
+              <Link to = "/experience">experience</Link>
+            </NavLink>
+          </NavItem>
+          
+          <NavItem>
+            <NavLink>
+              <Link to = "/projects">projects</Link>
+            </NavLink>
+          </NavItem>
+
+          <NavItem>
+            <NavLink>
+              <Link to = "/">resume</Link>
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+
+    /* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DEFAULT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
+    // <header
+    //   style={{
+    //     background: `rebeccapurple`,
+    //     marginBottom: `1.45rem`,
+    //   }}
+    // >
+    //   <div
+    //     style={{
+    //       margin: `0 auto`,
+    //       maxWidth: 960,
+    //       padding: `1.45rem 1.0875rem`,
+    //     }}
+    //   >
+    //     <h1 style={{ margin: 0 }}>
+    //       <Link
+    //         to="/"
+    //         style={{
+    //           color: `white`,
+    //           textDecoration: `none`,
+    //         }}
+    //       >
+    //         {siteTitle}
+    //       </Link>
+    //     </h1>
+    //   </div>
+    // </header>
+    )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import {Button} from "reactstrap"
+// import {Button} from "reactstrap"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -9,16 +9,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Button color='primary'>Bruhhh</Button>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Button color='secondary'>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </Button>
+    <Link to="/page-2/">Go to page 2</Link> <br />
     <br/>
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
