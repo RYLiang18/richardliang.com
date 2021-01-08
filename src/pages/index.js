bf41d49973daa5140fce9e9508fdc1f5e1c9ea88
RYLiang18 <richard.y.liang@gmail.com>
2021-01-07 18:12:10 -0800

    first commit - added reactstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import {Button} from "reactstrap"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,13 +9,17 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Button color='primary'>Bruhhh</Button>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Button color='secondary'>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+    </Button>
+    <br/>
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
